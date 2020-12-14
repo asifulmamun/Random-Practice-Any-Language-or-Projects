e22353096b6bffea3023401e0d6fb8e28e9d5f4b
asifulmamun <asifulmamun@gmail.com>
2020-12-14 13:49:10 +0600

    gTTS added

---
+++ textToSpech_gTTS.py
@@ -0,0 +1,14 @@
+"""
+Fist Install gtts with Command: pip install gTTS
+"""
+# import
+from gtts import gTTS
+
+# Assign Text
+myText = "আমি আল মামুন"
+
+# Text Input in Variable, lang #en or #bn is for language
+tts = gTTS(myText, lang="bn")
+
+# Save Txt as Audio
+tts.save('media/hello.mp3')
