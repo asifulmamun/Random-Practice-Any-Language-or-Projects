080225eb8ae6241b0856f8a480021d26b0129fe8
asifulmamun <asifulmamun@gmail.com>
2022-03-05 10:29:04 +0600

    simple auto press from keyboard script

---
+++ keyandenter.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+while True:
+    time.sleep(30)
+    pyautogui.typewrite('1998')
+    pyautogui.press('enter')
+    
+    pyautogui.typewrite('clear')
+    pyautogui.press('enter')
