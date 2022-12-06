61fc68e1a7efbedd2460396ae6628c79c47af61a
asifulmamun <asifulmamun@gmail.com>
2022-12-07 00:06:28 +0600

    corr

--- java/Get_input_from_user_then_add_double_number.java
+++ java/Get_input_from_user_then_add_double_number.java
@@ -1,7 +1,7 @@
 import java.text.DecimalFormat;
 import java.util.Scanner; // for input from user
 
-public class Try{
+public class Get_input_from_user_then_add_double_number{
     private static final DecimalFormat df = new DecimalFormat("0.00");
 
     public static void main(String[] args){
@@ -12,11 +12,12 @@ public class Try{
         double sum;
     
         
-        Scanner sc = new Scanner(System.in);
-        f1 = sc.nextDouble(); // data type and variable declare before
-        double f2 = sc.nextDouble(); // Getting input type declare data type
+        try (var sc = new Scanner(System.in)) {
+            f1 = sc.nextDouble(); // data type and variable declared before
+            double f2 = sc.nextDouble(); // Getting input type declare data type
 
-        sum = f1+f2;
+            sum = f1+f2;
+        }
         System.out.println(df.format(sum));
         System.out.printf("Sumi is = %.2f", sum); 
 
